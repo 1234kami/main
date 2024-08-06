85bc5e8ee12e5d247be73202b49e23beee1fbf56
kamila <takhminaacom@gmail.com>
2024-08-06 12:24:36 +0600

    commit

--- media/urls.py
+++ media/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('api/main/', include('main.urls')),
 ]
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 urlpatterns += yasg_urls
